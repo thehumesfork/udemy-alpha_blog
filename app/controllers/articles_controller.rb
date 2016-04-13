595796eb1213caefa0630af4f39f08bdbc4982a8
Rails UDemy Course <railsudemy@gmail.com>
2016-04-13 21:59:39 +0000

    Fixed create errors, added show definition and show template,and  handled errors.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,19 @@ class ArticlesController < ApplicationController
         @article = Article.new 
     end
     def create 
-        @article=Article.new(article_params) 
-        @article.save
-        redirect_to articles_show @article
+        @article = Article.new(article_params) 
+        if @article.save 
+        flash[:notice] = "Article saved successfully"
+        redirect_to article_path(@article)
+        else
+            render 'new'
+        end
     end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
     private
     def article_params
         params.require(:article).permit(:title,:description)
