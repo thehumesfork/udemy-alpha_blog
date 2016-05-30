2b93ce521d7dd8a3637443514e40cd08b1a29174
Rails UDemy Course <railsudemy@gmail.com>
2016-05-30 20:40:26 +0000

    Add edit, update, show, and index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController 
+    def index
+        
+        @articles = Article.all
+    
+    end
+   
     def new
         @article = Article.new 
     end
